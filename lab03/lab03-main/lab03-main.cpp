39568b3859fe73ebea186a565985249004bf62da
Your Name <you@example.com>
2023-07-09 22:16:45 +0300

    code: добавлено масштабирование столбцов

--- lab03/lab03-main/lab03-main.cpp
+++ lab03/lab03-main/lab03-main.cpp
@@ -157,6 +157,7 @@ void svg_rect(double x, double y, double width, double height, string stroke = "
 }
 
 void show_histogram_svg(const vector<int>& freak_sections, const int section_count) {
+
 	const int IMAGE_WIDTH = 400;
 	const int IMAGE_HEIGHT = 300;
 	const int TEXT_LEFT = 20;
@@ -164,13 +165,31 @@ void show_histogram_svg(const vector<int>& freak_sections, const int section_cou
 	const int BLOCK_WIDTH = 50;
 	const int BLOCK_HEIGHT = 30;
 	const int BLOCK_LEN = 10;
+	const double FREAK_LIMIT = 30;
 
-	svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
+	// Поиск максимальной частоты среди частот всех корзин
+	int max_freak = freak_sections[0];
+	for (int i = 1; i < section_count; i++)
+		if (freak_sections[i] > max_freak)
+			max_freak = freak_sections[i];
+
+	// Проверка, требуется ли выполнять масштабирование
+	bool scaling;
+	if (max_freak > FREAK_LIMIT)
+		scaling = true;
+	else
+		scaling = false;
+
 	double shift = 0;
+	svg_begin(IMAGE_WIDTH, IMAGE_HEIGHT);
 	for (int i = 0; i < section_count; i++) {
-		double section_width = BLOCK_LEN * freak_sections[i];
+		double section_width;
+		if (scaling)
+			section_width = BLOCK_LEN * freak_sections[i] / (max_freak / FREAK_LIMIT);
+		else
+			section_width = BLOCK_LEN * freak_sections[i];
 		svg_text(TEXT_LEFT, TEXT_BASELINE + shift, to_string(freak_sections[i]));
-		svg_rect(BLOCK_WIDTH, shift, section_width, BLOCK_HEIGHT);
+		svg_rect(BLOCK_WIDTH, shift, section_width, BLOCK_HEIGHT, "blue", "green");
 		shift += BLOCK_HEIGHT;
 	}
 	svg_end();
